d9d0a1e9c67af636f6a21ebdf9d8939cb6474c7d
ViktorNova <viktor@pinknoiseboys.com>
2015-05-06 13:31:18 -0700

    It works with no errors

--- rc.lua
+++ rc.lua
@@ -111,6 +111,7 @@ end
 -- for s = 1, screen.count() do
     -- Each screen has its own tag table.
 --    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout.layouts[1])
+--
 -- end
 -- }}}
 
