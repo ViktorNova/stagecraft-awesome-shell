af18a4213817aafa82cf222859b2afb66486c2c7
ViktorNova <viktor@pinknoiseboys.com>
2015-05-19 21:25:15 -0700

    Works on Arch again I didn't realize awesome-git was actually older than the awesome package..weird

--- rc.lua
+++ rc.lua
@@ -114,8 +114,8 @@ end
 --tags = {}
 --  for s = 1, screen.count() do
   -- Each screen has its own tag table.
---  tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
---tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout.layouts[1]) -- awesome-git version
+--      tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+--    awful.tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout[1]) -- awesome-git version
 --end
 
 
